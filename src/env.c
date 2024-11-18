566b93a537ee810d63ed38c5d457f0f44a8c239b
Romao Henrique Quilamiquiza <rquilami@c1r7s2.42luanda.com>
2024-11-18 12:22:48 +0100

    unset done

--- src/env.c
+++ src/env.c
@@ -6,7 +6,7 @@
 /*   By: rquilami <rquilami@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 11:22:22 by rquilami          #+#    #+#             */
-/*   Updated: 2024/11/17 14:04:06 by rquilami         ###   ########.fr       */
+/*   Updated: 2024/11/18 12:19:39 by rquilami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,12 @@ void env(t_env *ev, char *str)
 
 void	unset(char *key, t_env *ev)
 {
-    int i = 0;
-	int j = 0;
+    int i;
+	int j;
 
+	
+	i = 0;
+	j = 0;
     while (ev->env[i] != NULL)
 	{
         if (strncmp(ev->env[i], key, ft_strlen(key)) == 0 && ev->env[i][ft_strlen(key)] == '=')
@@ -47,7 +50,7 @@ void	unset(char *key, t_env *ev)
 		}
         i++;
     }
-	ev->env = realloc(ev->env, sizeof(char *) * ev->len--);
+	ev->env = realloc(ev->env, sizeof(char *) * i--);
 	if (ev->env == NULL && ev->len > 0)
 		return;
 }
